66826fe8e16dacc549225ef17052b497dd3bc76f
treksana <147983343+treksana@users.noreply.github.com>
2024-03-01 22:38:14 +0500

    Add files via upload

---
+++ task4on020324.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    int sum = 0;
+    while (number > 0) 
+    {
+        summ += number % 10;
+        number /= 10;
+    }
+    cout << summ << endl;
+}
